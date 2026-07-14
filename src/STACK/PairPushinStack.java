8ee0bd83517053bcc97094c5bc736c22d67a785b
Vijay-Nagadamudi <vijaynagadamudi525@gmail.com>
2026-07-14 16:59:45 +0530

    New File Added

---
+++ src/STACK/PairPushinStack.java
@@ -0,0 +1,33 @@
+package STACK;
+import java.util.Stack;
+public class PairPushinStack {
+    public static class Pair{
+        int num;
+        int idx;
+        Pair(int num,int idx){
+            this.num = num;
+            this.idx = idx;
+        }
+
+        @Override
+        public String toString(){
+            return "(" + num + "," + idx + ")";
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Stack<Pair> st = new Stack<>();
+        int[] arr = {100,80,60,70,75,85};
+        for(int i=0;i<arr.length;i++){
+            Pair p = new Pair(arr[i],i);
+            st.push(p);
+        }
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+    }
+
+
+
+}
