8c78a1d30c5c6073721dbd4d39025ab5eb249e5a
Vijay-Nagadamudi <vijaynagadamudi525@gmail.com>
2026-07-20 00:19:32 +0530

    New Stack file

---
+++ src/STACK/StockSpan.java
@@ -0,0 +1,29 @@
+package STACK;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class StockSpan {
+    static ArrayList<Integer> S_S(int[] arr){
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+        int x = arr.length;
+        st.push(arr[0]);
+        ans.add(arr[0]);
+
+        for(int i=1;i<x;i++){
+            while(st.size() > 0 && st.peek() < arr[i]) st.pop();
+            if(st.isEmpty()) ans.add(arr[i]);
+            else ans.add(st.peek());
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100, 80, 90, 120};
+        ArrayList<Integer> ans = S_S(arr);
+        System.out.println(ans);
+
+    }
+}
