67517aa1795a4ed52a7d31238dfcc1d584cd4658
Vijay-Nagadamudi <vijaynagadamudi525@gmail.com>
2026-07-25 21:49:44 +0530

    Changes

--- src/STACK/infix_solver.java
+++ src/STACK/infix_solver.java
@@ -1,16 +1,28 @@
 package STACK;
-import javax.xml.stream.events.Characters;
-import java.util.*;
+import java.util.Stack;
 public class infix_solver {
     public static void main(String[] args) {
-        String str = "9-5+3*4/6";
+        String str = "9-5+3*4-2";
         Stack <Integer> val = new Stack<>();
         Stack <Character> opr = new Stack<>();
-        for(int i=0; i<str.length();i++){
+        for(int i=0;i<str.length();i++){
             char ch = str.charAt(i);
-            int ascii = (int)ch;
-            if(ascii >= 48 && ascii <= 57) val.push(ascii - 48);
-            else if(opr.size() == 0) opr.push(ch);
+            int ascii = (int) ch;
+            if(ascii>=48 && ascii <= 57) val.push(ascii - 48);
+            else if(opr.size() == 0 || ch == '(' || opr.peek() == '(') opr.push(ch);
+            else if(ch == ')'){
+                while(opr.peek() != '('){
+                    int val2 = val.pop();
+                    int val1 = val.pop();
+                    if(opr.peek() == '+') val.push(val1 + val2);
+                    if(opr.peek() == '-') val.push(val1 - val2);
+                    if(opr.peek() == '*') val.push(val1 * val2);
+                    if(opr.peek() == '/') val.push(val1 / val2);
+                    opr.pop();;
+                }
+                opr.pop();
+            }
+
             else{
                 if(ch == '+' || ch == '-'){
                     int val2 = val.pop();
@@ -22,25 +34,23 @@ public class infix_solver {
                     opr.pop();
                     opr.push(ch);
                 }
-
-                else if(ch == '*' || ch == '/'){
-                    if(opr.peek() == '*' || opr.peek() == '/') {
+                if(ch == '*' || ch == '/'){
+                    if(opr.peek() == '*' || opr.peek() == '/'){
                         int val2 = val.pop();
                         int val1 = val.pop();
-                        if (opr.peek() == '*') val.push(val1 * val2);
-                        if (opr.peek() == '/') val.push(val1 / val2);
+                        if(opr.peek() == '*') val.push(val1 * val2);
+                        if(opr.peek() == '/') val.push(val1 / val2);
                         opr.pop();
                         opr.push(ch);
                     }
                     else opr.push(ch);
                 }
-                else opr.push(ch);
             }
         }
 
         while(val.size() > 1){
             int val2 = val.pop();
-            int val1  = val.pop();
+            int val1 = val.pop();
             if(opr.peek() == '+') val.push(val1 + val2);
             if(opr.peek() == '-') val.push(val1 - val2);
             if(opr.peek() == '*') val.push(val1 * val2);
@@ -49,7 +59,5 @@ public class infix_solver {
         }
 
         System.out.println(val.peek());
-
-
     }
 }
